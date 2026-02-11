abf721ade43f184a721f2c51f34eb0ae540c5199
Rock Clapps <rockclapps@gmail.com>
2026-02-10 21:59:44 -0500

    fixed installer on repeat uses

--- getAlternativeOllama.sh
+++ getAlternativeOllama.sh
@@ -3,6 +3,7 @@
 mkdir ollamaBin
 cd ollamaBin
 
+rm -r ollama-linux-amd64.tgz bin/ lib/
 wget https://github.com/ollama/ollama/releases/download/v0.12.11/ollama-linux-amd64.tgz
 
 tar -xzf ollama-linux-amd64.tgz
