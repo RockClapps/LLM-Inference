eede6ced0f49c451157a84ae27dc24841f49b60e
Rock Clapps <rockclapps@gmail.com>
2026-03-23 17:40:41 -0400

    reworked main.py to reddust_gender.py

---
+++ reddust_gender.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+import random
+import reddust_pushshift_manager as rpm
+
+# Parameters
+reddust_file = 'reddust/gender.csv'
+model = 'llama3.1:8b'
+prompt = '''Having read the above posts, please infer the gender of the person who
+wrote them. Please only respond with {options}.'''
+prompt_catagories = ["MALE", "FEMALE"]
+prompt_catagories_reddust_map = {"m": "FEMALE", "f": "FEMALE"}
+parquet_number_sample = 5
+max_posts = 10
+enforce_min_posts = False
+num_guesses = 10
+output_file = 'output.csv'
+errors_file = 'errors.csv'
+random_seed = None 
+
+rpm.collect_data(reddust_file, model, prompt, prompt_catagories,
+                 prompt_catagories_reddust_map, parquet_number_sample, max_posts,
+                 enforce_min_posts, num_guesses, output_file, errors_file,
+                 random_seed=random_seed)
